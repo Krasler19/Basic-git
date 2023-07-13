a9edc68b9ed18eb66ec0218f2e73e28c287d97f0
Krasler19 <4511601@gmail.com>
2023-07-13 18:43:52 +0300

    project

---
+++ JavaScript/lesson-10/script.js
@@ -0,0 +1,39 @@
+const words = [
+    "дерево",
+    "стіл",
+    "підлога",
+    "дім",
+    "велосипед",
+    "літак",
+    "машина",
+    "яблуко",
+];
+
+const word = words[Math.floor(Math.random() * words.length)];
+
+const answerArr = [];
+for (let i = 0; i < word.length; i += 1) {
+    answerArr.push("_");
+}
+while (answerArr.join("") !== word) {
+    alert(answerArr.join(" "));
+    const letter = prompt(
+        "Вгадайте букву, або натисніть cancel для завершення гри"
+    );
+    if (letter === "" || letter === null) {
+        break;
+    } else if (letter.length !== 1) {
+        alert("Будь ласка, введіть тільки одну букву");
+    }
+    else {
+        for (let i = 0; i < word.length; i += 1) {
+            if (word[i] === letter) {
+                answerArr[i] = letter;
+            }
+        }
+    }
+}
+
+if (answerArr.join("") === word) {
+    alert("Ви відгадали! Було загадано слово " + word);
+  }
